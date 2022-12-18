879273fcf780ce3ea706b4b719e253b27ae0bae6
TakumaNakahashi <115599704+tnakahashi@users.noreply.github.com>
2022-12-18 08:20:17 +0000

    [Update]顧客情報編集画面・更新機能追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,8 +3,17 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
   end
 
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to my_page_customers_path
+  end
 
   def customer_params
-     params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :postcode, :address, :phone_number, :encrypted_password)
+     params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :encrypted_password)
   end
 end
