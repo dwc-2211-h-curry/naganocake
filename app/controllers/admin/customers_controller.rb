d8cbc52fd5548f6bc47053777b7d4369c1b4b6cf
TakumaNakahashi <115599704+tnakahashi@users.noreply.github.com>
2022-12-20 08:03:17 +0000

    [Update]adminのcustomer関連の追加_レイアウト未調整、注文履歴へのリンク・ページネーション未追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,4 +1,28 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @all_customers = Customer.all
+  end
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email, :is_deleted)
   end
 end
