2c54625f24449f8af0dab99340ef1574022131fe
TakumaNakahashi <115599704+tnakahashi@users.noreply.github.com>
2022-12-20 03:43:28 +0000

    [Update]郵便番号及びカナ姓名のバリデーション追加_レイアウト未調整

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,10 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :postcode,
+    length: { is: 7 }
+  validates :last_name_kana,
+    format: { with: /\A([ｧ-ﾝﾞﾟァ-ヶー－]+）?)+\z/ }
+  validates :first_name_kana,
+    format: { with: /\A([ｧ-ﾝﾞﾟァ-ヶー－]+）?)+\z/ }
 end
