9bc9509424b1ad5af096839559a108960b862beb
EC2 Default User <ec2-user@ip-172-31-46-47.ap-northeast-1.compute.internal>
2022-12-21 07:12:24 +0000

    [Add]public/orders/confirm,controllers/public/orders

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,4 +1,6 @@
 class Public::OrdersController < ApplicationController
+  begfore_action :cart_item_nill, only: [:new, :create]
+
   def new
     @order = Order.new
     @shipping_addresses = ShippingAddress.all
@@ -27,6 +29,20 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    order = Order.new(order_params)
+    order.save
+    @cart_items = current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+      @order_details = OrderDetail.new
+      @order_details.order_id = order.id
+      @order_details.items_id = cart_item.item.id
+      @order_details.purchase_price = cart_item.item.add_tax_price
+      @order_details.product_status = 0
+      @order_details.quantity = cart_item.quantity
+      @order_details..save
+    end
+    CartItem.destroy_all
+    redirect_to complete_order_path
   end
 
   def index
@@ -39,5 +55,12 @@ class Public::OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:payment_way, :shipping_postal_code, :shipping_address, :shipping_name, :postage, :total, :customer_id, :order_status)
+  end
+
+  def cart_item_nill
+    cart_items = current_customer.cart_items
+    if cart_items.blank?
+      redirect_to cart_items_path
+    end
   end
 end
