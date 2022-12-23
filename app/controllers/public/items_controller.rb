814576b5ea9b0abd0efa34406070fd2028cf595e
TakumaNakahashi <115599704+tnakahashi@users.noreply.github.com>
2022-12-23 11:33:49 +0000

    [Fix]publicの商品一覧の商品件数を販売中のみに修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,7 +2,7 @@ class Public::ItemsController < ApplicationController
   def index
     @genres = Genre.all
     @items = Item.where(is_active: true).page(params[:page]).per(8) #8項目毎にページネート
-    @all_items = Item.all
+    @all_items = Item.all.where(is_active: true)
   end
 
   def show
