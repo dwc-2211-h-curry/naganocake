4a69a0398e3a988729fda6961da71a69186b0bda
TakumaNakahashi <115599704+tnakahashi@users.noreply.github.com>
2022-12-25 08:24:41 +0000

    [Fix]ジャンルのウィンドウ等を修正

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -7,6 +7,7 @@ class Public::GenresController < ApplicationController
   end
 
   def show
+    @genres = Genre.all
     @genre = Genre.find(params[:id])
     @items = Item.where("genre_id LIKE?", "#{@genre.id}").page(params[:page]).per(8) #8項目毎にページネート
     @all_items = Item.where("genre_id LIKE?", "#{@genre.id}")
